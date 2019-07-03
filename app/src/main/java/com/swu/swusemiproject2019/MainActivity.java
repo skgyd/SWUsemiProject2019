4fc4bfa52e3a17155cae4f7b10c60ecfd579db5b
Your Name <you@example.com>
2019-07-03 14:55:23 +0900

    first commit

--- app/src/main/java/com/swu/swusemiproject2019/MainActivity.java
+++ app/src/main/java/com/swu/swusemiproject2019/MainActivity.java
@@ -10,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //테스트 코드1
+
+        //commit 및 push 테스트 ~~~~~~~~~
     }
 }
